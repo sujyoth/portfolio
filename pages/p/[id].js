b3f1a10124c46e28c4637b5c4a0362cd5924c692
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-04-04 22:26:19 +0530

    Add react-markdown

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,11 +1,46 @@
 import Layout from '../../components/MyLayout'
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 const Post = props => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
+    <div className='markdown'>
+    <Markdown
+          source={`
+### ${props.show.name}
+
+![Batman Image](${props.show.image.medium})
+
+${props.show.summary.replace(/<[/]?[pb]>/g, '')}
+      `}
+        />
+    </div>
+    <style jsx global>
+      {`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+
+        .markdown img:hover {
+          opacity: 0.6;
+        }
+      `}
+    </style>
   </Layout>
 );
 

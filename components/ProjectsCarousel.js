13d0609c03bca8f75697fd121db4e2fc5adbe42d
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-04-18 03:24:33 +0530

    Separate CarouselItem

--- components/ProjectsCarousel.js
+++ components/ProjectsCarousel.js
@@ -1,5 +1,116 @@
 import { Carousel } from 'react-responsive-carousel';
 
+const CarouselItem = (props) => {
+  return (
+    <div className="slideContainer">
+      <div>
+        <div className="mainContainer">
+          <div className="mainText">{props.mainText}</div>
+          <div className="mainTextLine" />
+        </div>
+        <div className="subText">{props.subText}</div>
+      </div>
+      <style jsx>{`
+        .slideContainer {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+
+          width: 100%;
+          height: 100vh;
+          background: ${props.backgroundColor};
+
+          transition: background-color 0.3s ease;
+        }
+
+        .mainContainer {
+          padding-top: 14px;
+          margin-bottom: 20px;
+          display: inline-block;
+        }
+
+        .mainText {
+          font-size: 40px;
+          font-family: Anisette;
+          letter-spacing: 2px;
+          color: #fff;
+        }
+
+        .mainTextLine {
+          background: #fff;
+          width: 100%;
+          height: 8px;
+
+          animation: 1s ease rollFromLeft;
+          animation-delay: 0.4s;
+          animation-fill-mode: both;
+          transform-origin: left;
+        }
+
+        .subText {
+          font-size: 12px;
+          font-family: Brandon;
+          letter-spacing: 2px;
+          color: #fff;
+          text-transform: uppercase;
+          display: none;
+        }
+
+        @media screen and (min-width: 40em) {
+          .mainText {
+            font-size: 100px;
+          }
+
+          .mainTextLine {
+            height: 18px;
+          }
+
+          .subText {
+            display: block;
+          }
+        }
+
+        @keyframes rollFromLeft {
+          0% {
+            transform: scaleX(0);
+          }
+          100% {
+            transform: scaleX(100%);
+          }
+        }
+
+        @keyframes rollToLeft {
+          0% {
+            transform: scaleX(100%);
+          }
+          100% {
+            transform: scaleX(0);
+          }
+        }
+
+        @keyframes fadeIn {
+          0% {
+            opacity: 0%;
+          }
+          100% {
+            opacity: 100%;
+          }
+        }
+
+        @keyframes fadeOut {
+          0% {
+            opacity: 100%;
+          }
+          100% {
+            opacity: 0%;
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
+
 export default class ProjectsCarousel extends React.Component {
   constructor(props) {
     super(props);
@@ -54,36 +165,25 @@ export default class ProjectsCarousel extends React.Component {
           selectedItem={this.state.currentSlide}
           onChange={this.updateCurrentSlide}
         >
-          <div className="slideContainer">
-            <div>
-              <div className="mainContainer">
-                <div className="mainText">vote</div>
-                <div className="mainTextLine line0" />
-              </div>
-              <div className="subText text0">Opinion-based question generator</div>
-            </div>
-          </div>
-          <div className="slideContainer">
-            <div className="mainContainer">
-              <div className="mainText">BookwoRM</div>
-              <div className="mainTextLine line1" />
-            </div>
-            <div className="subText text1">Local book-sharing app</div>
-          </div>
-          <div className="slideContainer">
-            <div className="mainContainer">
-              <div className="mainText">Lyricvis</div>
-              <div className="mainTextLine line2" />
-            </div>
-            <div className="subText text2">Song suggestion and lyrics fetching app</div>
-          </div>
-          <div className="slideContainer">
-            <div className="mainContainer">
-              <div className="mainText">roVer</div>
-              <div className="mainTextLine line3" />
-            </div>
-            <div className="subText text3">Surveillance Car based on Raspberry Pi</div>
-          </div>
+          <CarouselItem 
+            mainText="vote" 
+            subText="Opinion-based question generator"
+            backgroundColor={this.state.backgroundColor} />
+          <CarouselItem 
+            mainText="BookwoRM" 
+            subText="Local book-sharing app" 
+            backgroundColor={this.state.backgroundColor} />
+
+          <CarouselItem  
+            mainText="Lyricvis" 
+            subText="Song suggestion and lyrics fetching app" 
+            backgroundColor={this.state.backgroundColor} />
+
+          <CarouselItem 
+            mainText="roVer" 
+            subText="Surveillance Car based on Raspberry Pi" 
+            backgroundColor={this.state.backgroundColor} />
+
         </Carousel>
 
         <style jsx>{`
@@ -97,108 +197,6 @@ export default class ProjectsCarousel extends React.Component {
 
             width: 100%;
             height: 100%;
-          }
-
-          .slideContainer {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-
-            width: 100%;
-            height: 100vh;
-            background: ${this.state.backgroundColor};
-
-            transition: background-color 0.3s ease;
-          }
-
-          .mainContainer {
-            padding-top: 14px;
-            margin-bottom: 20px;
-            display: inline-block;
-          }
-
-          .mainText {
-            font-size: 40px;
-            font-family: Anisette;
-            letter-spacing: 2px;
-            color: #fff;
-          }
-
-          .mainTextLine {
-            background: #fff;
-            width: 100%;
-            height: 8px;
-
-            animation: 1s ease rollFromLeft;
-            animation-delay: 0.4s;
-            animation-fill-mode: both;
-            transform-origin: left;
-          }
-
-          .subText {
-            font-size: 12px;
-            font-family: Brandon;
-            letter-spacing: 2px;
-            color: #fff;
-            text-transform: uppercase;
-            display: none;
-          }
-
-          @media screen and (min-width: 40em) {
-            .mainText {
-              font-size: 100px;
-            }
-
-            .mainTextLine {
-              height: 18px;
-            }
-
-            .subText {
-              display: block;
-            }
-          }
-
-          @keyframes colorChange {
-            100% {
-              background: ${this.state.backgroundColor};
-            }
-          }
-
-          @keyframes rollFromLeft {
-            0% {
-              transform: scaleX(0);
-            }
-            100% {
-              transform: scaleX(100%);
-            }
-          }
-
-          @keyframes rollToLeft {
-            0% {
-              transform: scaleX(100%);
-            }
-            100% {
-              transform: scaleX(0);
-            }
-          }
-
-          @keyframes fadeIn {
-            0% {
-              opacity: 0%;
-            }
-            100% {
-              opacity: 100%;
-            }
-          }
-
-          @keyframes fadeOut {
-            0% {
-              opacity: 100%;
-            }
-            100% {
-              opacity: 0%;
-            }
           }
         `}</style>
       </div>

0a511e6875ceb66eb3cfa942608932cf16f34cb6
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-04-18 20:34:42 +0530

    Add automatic eye movement

--- components/Eye.js
+++ components/Eye.js
@@ -1,22 +1,50 @@
-const Eye = (props) => {
-  return (
-        <svg viewBox="0 0 350 250">
-          <defs>
-            <clipPath id="eye">
-              <path
-                d="
+class Eye extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eyePosX: 175,
+      eyePosY: 180
+    }
+
+    setInterval(() => {
+      this.setState({
+        eyePosX: this.getRandX(),
+        eyePosY: this.getRandY()
+      });
+    }, 1000 + Math.random() * 4000)
+  }
+  getRandX = () => {
+    const minX = 140;
+    const maxX = 205;
+    return (minX + Math.random() * (maxX - minX));
+  }
+
+  getRandY = () => {
+    const minY = 140;
+    const maxY = 205;
+    return (minY + Math.random() * (maxY - minY));
+  }
+
+  render() {
+    return (
+      <svg viewBox="0 0 350 250">
+        <defs>
+          <clipPath id="eye">
+            <path
+              d="
              M 0 200 
              Q 200 0 350 200 
              Q 150 300 0 200 Z
             "
-              />
-            </clipPath>
-          </defs>
-          <g id="eyeBall">
-            <rect width="3500" height="250" fill="#ffccf7" />
-            <circle cx="175" cy="180" r="60" fill="#5454ff" />
-          </g>
-          <style jsx>{`
+            />
+          </clipPath>
+        </defs>
+        <g id="eyeBall">
+          <rect width="3500" height="250" fill="#ffccf7" />
+          <circle cx={this.state.eyePosX} cy={this.state.eyePosY} r="75" fill="#5454ff" />
+        </g>
+        <style jsx>{`
           svg path {
             transition: 1s;
           }
@@ -25,13 +53,17 @@ const Eye = (props) => {
             d: path("M0,200 Q200,180,350,200 Q200,180,0,200 Z");
           }
 
+          svg circle {
+            transition: 3s;
+          }
+
           #eyeBall {
             clip-path: url(#eye);
           }
         `}</style>
-        </svg>
-        
-  );
+      </svg>
+    );
+  }
 }
 
 export default Eye;

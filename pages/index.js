3dfdcee3d78fa606fd369570f69cf298e6694bc8
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-05-07 22:13:46 +0530

    Fix OG image tag

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Index = props => {
       <Head>
         <title key="title">Sujyoth Karkera</title>
         <meta key="og:title" property="og:title" content="Sujyoth Karkera" />
-        <meta key="og:image" property='og:image' content="/public/images/home.png" />
+        <meta key="og:image" property='og:image' content="/images/home.png" />
         <meta key='og:description' property='og:description' content="A website to showcase my projects." />
         <meta key='og:url' property='og:url' content="https://sujyoth.now.sh" />
       </Head>

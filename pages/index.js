a6d098755223985e23f0f088124f1d1c71981232
Sujyoth Karkera <sujyothkarkera99@gmail.com>
2020-05-07 22:09:24 +0530

    Add SEO-friendly meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Layout from '../components/MyLayout';
 import ProjectsCarousel from '../components/ProjectsCarousel';
+import Head from 'next/head';
 
 const Index = props => {
   return (
-    <Layout>
-      <ProjectsCarousel />
-    </Layout>
+    <>
+      <Head>
+        <title key="title">Sujyoth Karkera</title>
+        <meta key="og:title" property="og:title" content="Sujyoth Karkera" />
+        <meta key="og:image" property='og:image' content="/public/images/home.png" />
+        <meta key='og:description' property='og:description' content="A website to showcase my projects." />
+        <meta key='og:url' property='og:url' content="https://sujyoth.now.sh" />
+      </Head>
+      <Layout>
+        <ProjectsCarousel />
+      </Layout>
+    </>
   );
 }
 
